d7d6e3b2a808ff6a15d2cd30bbb26df2b60118f7
Thaoky <77798624+Thaoky@users.noreply.github.com>
2024-05-10 09:53:27 +0200

    Release candidate for Altoholic 10.2.013

---
+++ DataStore_Inventory/API/GuildComm.lua
@@ -0,0 +1,222 @@
+--[[ 
+This file keeps track of the guild communication when exchanging player inventories
+--]]
+
+local addonName, addon = ...
+local guilds
+local options
+
+local DataStore = DataStore
+local TableInsert, TableConcat, format, strsplit, pairs, type, tonumber, time = table.insert, table.concat, format, strsplit, pairs, type, tonumber, time
+local UnitName = UnitName
+
+local commPrefix = "DS_Inv"
+local L = DataStore:GetLocale(addonName)
+
+-- Message types
+local MSG_SEND_AIL								= 1	-- Send AIL at login
+local MSG_AIL_REPLY								= 2	-- reply
+local MSG_EQUIPMENT_REQUEST					= 3	-- request equipment ..
+local MSG_EQUIPMENT_TRANSFER					= 4	-- .. and send the data
+
+-- *** Utility functions ***
+local function GetThisGuild()
+	local guildID = DataStore:GetCharacterGuildID(DataStore.ThisCharKey)
+	return guildID and guilds[guildID] 
+end
+
+local function GetMemberKey(guild, member)
+	-- returns the appropriate key to address a guild member. 
+	--	Either it's a known alt ==> point to the characters table
+	--	Or it's a guild member ==> point to the guild table
+	local main = DataStore:GetNameOfMain(member)
+	
+	if main and main == UnitName("player") then
+		local key = format("%s.%s.%s", DataStore.ThisAccount, DataStore.ThisRealm, member)
+		local id = DataStore:GetCharacterID(key)
+		
+		return DataStore_Inventory_Characters[id]
+	end
+	
+	return guild.Members[member]
+end
+
+local function GetAIL(alts)
+	-- alts = list of alts in the same guild, same realm, same account, pipe-delimited : "alt1|alt2|alt3..."
+	--	usually provided by the main datastore module, but can also be built manually
+	local out = {}
+	
+	local character = DataStore:GetCharacter()	-- this character
+	local ail = DataStore:GetAverageItemLevel(character)
+	TableInsert(out, format("%s:%d", UnitName("player"), ail))
+
+	if strlen(alts) > 0 then
+		for _, name in pairs( { strsplit("|", alts) }) do	-- then all his alts
+			character = DataStore:GetCharacter(name)
+			
+			if character then
+				ail = DataStore:GetAverageItemLevel(character)
+				
+				if ail then
+					TableInsert(out, format("%s:%d", name, ail))
+				end
+			end
+		end
+	end
+
+	return TableConcat(out, "|")
+end
+
+local function SaveAIL(sender, ailList)
+	local thisGuild = GetThisGuild()
+	if not thisGuild then return end
+	
+	thisGuild.Members = thisGuild.Members or {}
+	
+	for _, ailChar in pairs( { strsplit("|", ailList) }) do	-- "char:ail | char:ail | ..."
+		local name, ail = strsplit(":", ailChar)
+		
+		if name and ail then
+			thisGuild.Members[name].averageItemLvl = tonumber(ail)
+		end
+	end
+end
+
+local function ClearGuildInventories()
+	local thisGuild = GetThisGuild()
+	if thisGuild then
+		wipe(thisGuild.Members)
+	end
+end
+
+
+-- ** Mixins **
+local sentRequests		-- recently sent requests
+
+local function _RequestGuildMemberEquipment(member)
+	-- requests the equipment of a given character (alt or main)
+	local player = UnitName("player")
+	local main = DataStore:GetNameOfMain(member)
+	
+	if not main then 		-- player is offline, check if his equipment is in the DB
+		local thisGuild = GetThisGuild()
+		
+		if thisGuild and thisGuild.Members[member] then		-- player found
+			if thisGuild.Members[member].Inventory then		-- equipment found
+				DataStore:Broadcast("DATASTORE_PLAYER_EQUIPMENT_RECEIVED", player, member)
+				return
+			end
+		end
+	end
+	
+	if main == player then	-- if player requests the equipment of one of own alts, process the request locally, using the network works fine, but let's save the traffic.
+		-- trigger the same event, _GetGuildMemberInventoryItem will take care of picking the data in the right place
+		DataStore:Broadcast("DATASTORE_PLAYER_EQUIPMENT_RECEIVED", player, member)
+		return
+	end
+	
+	-- prevent spamming remote players with too many requests
+	sentRequests = sentRequests or {}
+	
+	if sentRequests[main] and ((time() - sentRequests[main]) < 5) then		-- if there's a known timestamp , and it was sent less than 5 seconds ago .. exit
+		return
+	end
+	
+	sentRequests[main] = time()		-- timestamp of the last request sent to this player
+	DataStore:GuildWhisper(commPrefix, main, MSG_EQUIPMENT_REQUEST, member)
+end
+
+local function _GetGuildMemberInventoryItem(guild, member, slotID)
+	local character = GetMemberKey(guild, member)
+	
+	if character then
+		return character.Inventory[slotID]
+	end
+end
+
+local function _GetGuildMemberAverageItemLevel(guild, member)
+	local character = GetMemberKey(guild, member)
+
+	if character then
+		return character.averageItemLvl
+	end
+end
+
+
+-- *** Guild Comm ***
+local function OnGuildAltsReceived(self, sender, alts)
+	if sender == UnitName("player") and options.BroadcastAiL then				-- if I receive my own list of alts in the same guild, same realm, same account..
+		DataStore:GuildBroadcast(commPrefix, MSG_SEND_AIL, GetAIL(alts))	-- ..then broacast AIL
+	end
+end
+
+local commCallbacks = {
+	[MSG_SEND_AIL] = function(sender, ail)
+			local player = UnitName("player")
+			if sender ~= player then						-- don't send back to self
+				local alts = DataStore:GetGuildMemberAlts(player)			-- get my own alts
+				if alts and options.BroadcastAiL then
+					DataStore:GuildWhisper(commPrefix, sender, MSG_AIL_REPLY, GetAIL(alts))		-- .. and send them back
+				end
+			end
+			SaveAIL(sender, ail)
+		end,
+	[MSG_AIL_REPLY] = function(sender, ail)
+			SaveAIL(sender, ail)
+		end,
+	[MSG_EQUIPMENT_REQUEST] = function(sender, character)
+			if options.EquipmentRequestNotification then
+				addon:Print(format(L["%s is inspecting %s"], sender, character))
+			end
+	
+			local key = DataStore:GetCharacter(character)	-- this realm, this account
+			if key then
+				DataStore:GuildWhisper(commPrefix, sender, MSG_EQUIPMENT_TRANSFER, character, DataStore:GetInventory(key))
+			end
+		end,
+	[MSG_EQUIPMENT_TRANSFER] = function(sender, character, equipment)
+			local thisGuild = GetThisGuild()
+			if thisGuild then
+				thisGuild.Members[character].Inventory = equipment
+				thisGuild.Members[character].lastUpdate = time()
+				DataStore:Broadcast("DATASTORE_PLAYER_EQUIPMENT_RECEIVED", sender, character)
+			end
+		end,
+}
+
+DataStore:OnAddonLoaded(addonName, function() 
+	DataStore:RegisterTables({
+		addon = addon,
+		guildTables = {
+			["DataStore_Inventory_Guilds"] = {
+				GetGuildMemberInventoryItem = _GetGuildMemberInventoryItem,
+				GetGuildMemberAverageItemLevel = _GetGuildMemberAverageItemLevel,
+			},
+		},
+	})
+	
+	guilds = DataStore_Inventory_Guilds
+	
+	local guildID = DataStore:GetCharacterGuildID(DataStore.ThisCharKey)
+	if guildID then
+		guilds[guildID] = guilds[guildID] or {}				-- Create the guild
+	end
+
+	DataStore:SetGuildCommCallbacks(commPrefix, commCallbacks)
+	DataStore:ListenTo("DATASTORE_GUILD_ALTS_RECEIVED", OnGuildAltsReceived)
+	DataStore:OnGuildComm(commPrefix, DataStore:GetGuildCommHandler())
+	
+	DataStore:RegisterMethod(addon, "RequestGuildMemberEquipment", _RequestGuildMemberEquipment)
+end)
+
+DataStore:OnPlayerLogin(function()
+	options = DataStore:SetDefaults("DataStore_Inventory_Options", {
+		BroadcastAiL = true,							-- Broadcast professions at login or not
+		AutoClearGuildInventory = false,			-- Automatically clear guild members' inventory at login
+		EquipmentRequestNotification = false,	-- Get a warning when someone requests my equipment
+	})
+
+	if options.AutoClearGuildInventory then
+		ClearGuildInventories()
+	end
+end)
--- DataStore_Inventory/DataStore_Inventory.lua
+++ DataStore_Inventory/DataStore_Inventory.lua
@@ -6,28 +6,21 @@ if not DataStore then return end
 
 local addonName, addon = ...
 local thisCharacter
-local guilds
 local setInfo
 local collectedSets
 local appearancesCounters
-local options
+
 
 local DataStore = DataStore
 local TableInsert, TableConcat, strfind, format, strsplit, pairs, type, tonumber, select, time = table.insert, table.concat, string.find, format, strsplit, pairs, type, tonumber, select, time
-local GetAverageItemLevel, GetInventoryItemLink, GetItemInfo, GetItemInfoInstant, UnitClass, UnitName = GetAverageItemLevel, GetInventoryItemLink, GetItemInfo, GetItemInfoInstant, UnitClass, UnitName
+local GetAverageItemLevel, GetInventoryItemLink, GetItemInfo, GetItemInfoInstant, UnitClass = GetAverageItemLevel, GetInventoryItemLink, GetItemInfo, GetItemInfoInstant, UnitClass
 local C_TransmogCollection, C_TransmogSets = C_TransmogCollection, C_TransmogSets
 
-local commPrefix = "DS_Inv"
 local isRetail = (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE)
 
 local L = DataStore:GetLocale(addonName)
 local bit64 = LibStub("LibBit64")
 
--- Message types
-local MSG_SEND_AIL								= 1	-- Send AIL at login
-local MSG_AIL_REPLY								= 2	-- reply
-local MSG_EQUIPMENT_REQUEST					= 3	-- request equipment ..
-local MSG_EQUIPMENT_TRANSFER					= 4	-- .. and send the data
 
 -- *** Utility functions ***
 local NUM_EQUIPMENT_SLOTS = 30			-- 30 slots when counting the new profession tools added in 10.0
@@ -38,75 +31,6 @@ local function IsEnchanted(link)
 	if not strfind(link, "item:%d+:0:0:0:0:0:0:%d+:%d+:0:0") then	-- 7th is the UniqueID, 8th LinkLevel which are irrelevant
 		-- enchants/jewels store values instead of zeroes in the link, if this string can't be found, there's at least one enchant/jewel
 		return true
-	end
-end
-
-local function GetThisGuild()
-	local guildID = DataStore:GetCharacterGuildID(DataStore.ThisCharKey)
-	return guildID and guilds[guildID] 
-end
-
-local function GetMemberKey(guild, member)
-	-- returns the appropriate key to address a guild member. 
-	--	Either it's a known alt ==> point to the characters table
-	--	Or it's a guild member ==> point to the guild table
-	local main = DataStore:GetNameOfMain(member)
-	
-	if main and main == UnitName("player") then
-		local key = format("%s.%s.%s", DataStore.ThisAccount, DataStore.ThisRealm, member)
-		local id = DataStore:GetCharacterID(key)
-		
-		return DataStore_Inventory_Characters[id]
-	end
-	
-	return guild.Members[member]
-end
-
-local function GetAIL(alts)
-	-- alts = list of alts in the same guild, same realm, same account, pipe-delimited : "alt1|alt2|alt3..."
-	--	usually provided by the main datastore module, but can also be built manually
-	local out = {}
-	
-	local character = DataStore:GetCharacter()	-- this character
-	local ail = DataStore:GetAverageItemLevel(character)
-	TableInsert(out, format("%s:%d", UnitName("player"), ail))
-
-	if strlen(alts) > 0 then
-		for _, name in pairs( { strsplit("|", alts) }) do	-- then all his alts
-			character = DataStore:GetCharacter(name)
-			
-			if character then
-				ail = DataStore:GetAverageItemLevel(character)
-				
-				if ail then
-					TableInsert(out, format("%s:%d", name, ail))
-				end
-			end
-		end
-	end
-
-	return TableConcat(out, "|")
-end
-
-local function SaveAIL(sender, ailList)
-	local thisGuild = GetThisGuild()
-	if not thisGuild then return end
-	
-	thisGuild.Members = thisGuild.Members or {}
-	
-	for _, ailChar in pairs( { strsplit("|", ailList) }) do	-- "char:ail | char:ail | ..."
-		local name, ail = strsplit(":", ailChar)
-		
-		if name and ail then
-			thisGuild.Members[name].averageItemLvl = tonumber(ail)
-		end
-	end
-end
-
-local function ClearGuildInventories()
-	local thisGuild = GetThisGuild()
-	if thisGuild then
-		wipe(thisGuild.Members)
 	end
 end
 
@@ -367,57 +291,6 @@ local function _IterateInventory(character, callback)
 	end
 end
 
-local sentRequests		-- recently sent requests
-
-local function _RequestGuildMemberEquipment(member)
-	-- requests the equipment of a given character (alt or main)
-	local player = UnitName("player")
-	local main = DataStore:GetNameOfMain(member)
-	
-	if not main then 		-- player is offline, check if his equipment is in the DB
-		local thisGuild = GetThisGuild()
-		
-		if thisGuild and thisGuild.Members[member] then		-- player found
-			if thisGuild.Members[member].Inventory then		-- equipment found
-				DataStore:Broadcast("DATASTORE_PLAYER_EQUIPMENT_RECEIVED", player, member)
-				return
-			end
-		end
-	end
-	
-	if main == player then	-- if player requests the equipment of one of own alts, process the request locally, using the network works fine, but let's save the traffic.
-		-- trigger the same event, _GetGuildMemberInventoryItem will take care of picking the data in the right place
-		DataStore:Broadcast("DATASTORE_PLAYER_EQUIPMENT_RECEIVED", player, member)
-		return
-	end
-	
-	-- prevent spamming remote players with too many requests
-	sentRequests = sentRequests or {}
-	
-	if sentRequests[main] and ((time() - sentRequests[main]) < 5) then		-- if there's a known timestamp , and it was sent less than 5 seconds ago .. exit
-		return
-	end
-	
-	sentRequests[main] = time()		-- timestamp of the last request sent to this player
-	DataStore:GuildWhisper(commPrefix, main, MSG_EQUIPMENT_REQUEST, member)
-end
-
-local function _GetGuildMemberInventoryItem(guild, member, slotID)
-	local character = GetMemberKey(guild, member)
-	
-	if character then
-		return character.Inventory[slotID]
-	end
-end
-
-local function _GetGuildMemberAverageItemLevel(guild, member)
-	local character = GetMemberKey(guild, member)
-
-	if character then
-		return character.averageItemLvl
-	end
-end
-
 local function _GetSetIcon(setID)
 	
 	-- *** now loaded through a scan, should not be necessary anymore. ***
@@ -465,48 +338,6 @@ local function _IsSetItemCollected(setID, sourceID)
 end
 
 
-
--- *** Guild Comm ***
-local function OnGuildAltsReceived(self, sender, alts)
-	if sender == UnitName("player") and options.BroadcastAiL then				-- if I receive my own list of alts in the same guild, same realm, same account..
-		DataStore:GuildBroadcast(commPrefix, MSG_SEND_AIL, GetAIL(alts))	-- ..then broacast AIL
-	end
-end
-
-local commCallbacks = {
-	[MSG_SEND_AIL] = function(sender, ail)
-			local player = UnitName("player")
-			if sender ~= player then						-- don't send back to self
-				local alts = DataStore:GetGuildMemberAlts(player)			-- get my own alts
-				if alts and options.BroadcastAiL then
-					DataStore:GuildWhisper(commPrefix, sender, MSG_AIL_REPLY, GetAIL(alts))		-- .. and send them back
-				end
-			end
-			SaveAIL(sender, ail)
-		end,
-	[MSG_AIL_REPLY] = function(sender, ail)
-			SaveAIL(sender, ail)
-		end,
-	[MSG_EQUIPMENT_REQUEST] = function(sender, character)
-			if options.EquipmentRequestNotification then
-				addon:Print(format(L["%s is inspecting %s"], sender, character))
-			end
-	
-			local key = DataStore:GetCharacter(character)	-- this realm, this account
-			if key then
-				DataStore:GuildWhisper(commPrefix, sender, MSG_EQUIPMENT_TRANSFER, character, DataStore:GetInventory(key))
-			end
-		end,
-	[MSG_EQUIPMENT_TRANSFER] = function(sender, character, equipment)
-			local thisGuild = GetThisGuild()
-			if thisGuild then
-				thisGuild.Members[character].Inventory = equipment
-				thisGuild.Members[character].lastUpdate = time()
-				DataStore:Broadcast("DATASTORE_PLAYER_EQUIPMENT_RECEIVED", sender, character)
-			end
-		end,
-}
-
 DataStore:OnAddonLoaded(addonName, function()
 	DataStore:RegisterModule({
 		addon = addon,
@@ -526,28 +357,15 @@ DataStore:OnAddonLoaded(addonName, function()
 				IterateInventory = isRetail and _IterateInventory,
 			},
 		},
-		guildTables = {
-			["DataStore_Inventory_Guilds"] = {
-				GetGuildMemberInventoryItem = _GetGuildMemberInventoryItem,
-				GetGuildMemberAverageItemLevel = _GetGuildMemberAverageItemLevel,
-			},
-		},
+
 	})
 	
 	thisCharacter = DataStore:GetCharacterDB("DataStore_Inventory_Characters", true)
 	thisCharacter.Inventory = thisCharacter.Inventory or {}
 	
-	guilds = DataStore_Inventory_Guilds
 	setInfo = DataStore_Inventory_SetInfo
 	collectedSets = DataStore_Inventory_CollectedSets
 	appearancesCounters = DataStore_Inventory_AppearancesCounters
-	
-
-	DataStore:SetGuildCommCallbacks(commPrefix, commCallbacks)
-	DataStore:ListenTo("DATASTORE_GUILD_ALTS_RECEIVED", OnGuildAltsReceived)
-	DataStore:OnGuildComm(commPrefix, DataStore:GetGuildCommHandler())
-	
-	DataStore:RegisterMethod(addon, "RequestGuildMemberEquipment", _RequestGuildMemberEquipment)
 	
 	-- Stop here for non-retail
 	if not isRetail then return end
@@ -559,12 +377,6 @@ DataStore:OnAddonLoaded(addonName, function()
 end)
 
 DataStore:OnPlayerLogin(function()
-	options = DataStore_Inventory_Options
-	
-	options.AutoClearGuildInventory = options.AutoClearGuildInventory or false					-- Automatically clear guild members' inventory at login
-	options.BroadcastAiL = options.BroadcastAiL or true												-- Broadcast professions at login or not
-	options.EquipmentRequestNotification = options.EquipmentRequestNotification or false	-- Get a warning when someone requests my equipment
-	
 	addon:ListenTo("PLAYER_ALIVE", OnPlayerAlive)
 	addon:ListenTo("PLAYER_EQUIPMENT_CHANGED", OnPlayerEquipmentChanged)
 	
@@ -575,10 +387,6 @@ DataStore:OnPlayerLogin(function()
 	else
 		addon:ListenTo("GET_ITEM_INFO_RECEIVED", OnGetItemInfoReceived)
 		addon:SetupOptions()
-	end
-	
-	if options.AutoClearGuildInventory then
-		ClearGuildInventories()
 	end
 end)
 
